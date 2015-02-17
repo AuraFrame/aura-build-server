aeadc6b166d23b8c431cb62863a6820fc25b5be8
아우라 <my@devconf.cf>
2015-02-17 14:27:01 +0900

    cFileSystem 라이브러리 최초 커밋
    cDash 컨트롤러에 필요한 라이브러리입니다.

--- controllers/cDash/cFileSystem.js
+++ controllers/cDash/cFileSystem.js
@@ -3,3 +3,24 @@
 
 // 기본 라이브러리인 fs를 참조합니다. ( 2015-02-17 오후 02:12 )
 var fs = require("fs");
+
+var cFileSystem = (function(ef){
+    // 일반 텍스트 파일을 읽어내는 함수입니다. ( 2015-02-17 오후 02:17 )
+    this.readFile = function(url){
+        // 파일의 존재여부를 확인합니다.
+        if(ef.existsSync(url) == true)
+            return ef.readFileSync(url); // 파일이 존재한다면, readFileSync함수를 사용하여 파일을 읽고, 그 값을 반환합니다.
+        else
+            return null; // 파일이 존재하지 않을경우, null을 반환합니다.
+    };
+
+    // 데이터를 읽어들여 Object 형식이나 String 형식으로 반환합니다.
+    this.JSON = function(data){
+        if(typeof data === "object") // data의 형식이 Object일 경우, encode한 값을 반환합니다.
+            return JSON.stringify(data);
+        else if(typeof data === "string") // data의 형식이 String일 경우, deconde한 값을 반환합니다.
+            return JSON.parse(data);
+    }
+})(fs);
+
+module.exports = cFileSystem;
