a28413d546db8fa91cf933270b96de07c8650c2f
아우라 <my@devconf.cf>
2015-02-17 15:30:38 +0900

    gOpt 라이브러리 수정
    필요없는 변수를 삭제하였으며,
    파싱을 정규식을 사용하는 방식으로 변경.

--- controllers/gOpt.js
+++ controllers/gOpt.js
@@ -1,23 +1,38 @@
 // 초기화에 필요한 변수를 생성합니다. ( 2015-02-17 오후 01:52 )
-var get-options;
-var parsed-options;
-var __matches = null;
+var parsed_options;
 
-// 축약함수를 정의합니다. ( 2015-02-17 오후 01:53 )
-// Object 형식의 변수에 __hasProp을 사용합니다.
-var  __hasProp = {}.hasOwnProperty;
-
-// parsed-options 변수에 Object 개체를 대입합니다. ( 2015-02-17 오후 01:56 )
-parsed-options = (function(){
+// parsed_options 변수에 Object 개체를 대입합니다. ( 2015-02-17 오후 01:56 )
+parsed_options = new (function(){
     // oThis 변수가 this를 가리키도록 정의합니다. ( 2015-02-17 오후 01:58 )
     var oThis = this;
+    this.opt = {};
 
-    // ParsedOption이라는 함수를 정의합니다. ( 2015-02-17 오후 01:58 )
-    // 각 패러미터들은 parsed-options에 저장됩니다.
-    // [Array] args - Command Arguments를 모두 파싱한 뒤, 그 값을 전달받습니다.
-    // [Object] options - CA에서 옵션 내용을 모두 파싱한 뒤, 그 값을 전달받습니다.
-    function ParsedOption(args, options){
-        oThis.argv = args;
-        oThis.option = options;
+    function ArrayIndexOf(array, data){
+        for(var i = 0, j = array.length; i < j; i++){
+            if (array[i].match(data) != null)
+                return i;
+        }
+        return -1;
     }
+
+    // Initialize 함수는 패러미터로 전달받은 Object를 바탕으로 CA를 파싱합니다. ( 2015-02-17 오후 02:44 )
+    // [Object] opts - 필요한 패러미터의 옵션을 전달받습니다.
+    //                              arguments와 options 2가지의 루트키가 존재해야 하며, 그 안에는 또 다른 Object 개체가 필요합니다.
+    this.initialize = function(opts){
+        var argv = process.argv.slice(2); // process.argv는 명령문 모두를 출력하므로, 1, 2번째 값은 삭제합니다.
+        var data = {}; // 반환할 값이 담길 변수를 미리 선언합니다.
+
+        for(var x in opts.options){
+            var d; data[x] = "";
+            d = ArrayIndexOf(argv, new RegExp("--" + opts.options[x][1] + "=(.*)", "g"));
+            if(d != -1){ data[x] = argv[d].replace("--" + opts.options[x][1] + "=", ""); continue; }
+            d = ArrayIndexOf(argv, new RegExp("-" + opts.options[x][0] + "$", "g"));
+            if(d != -1){ data[x] = argv[d + 1]; continue; }
+        }
+        oThis.opt = data;
+
+        return oThis;
+    };
 });
+
+module.exports = parsed_options;
