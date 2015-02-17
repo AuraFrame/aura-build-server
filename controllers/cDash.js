377fbbe86e4c792371fb73802d2793c92e2adbb6
아우라 <my@devconf.cf>
2015-02-17 14:26:29 +0900

    CDash 컨트롤러 최초 커밋

--- controllers/cDash.js
+++ controllers/cDash.js
@@ -1,2 +1,12 @@
 // cDash를 사용하기 위한 라이브러리를 참조합니다. ( 2015-02-17 오후 02:08 )
 var fs = require("./cDash/cFileSystem");
+
+var cDash = (function(ef){
+    this.get = function(conf_url){
+        var data = ef.readFile("configs/" + conf_url);
+        if(data !== null)
+            return ef.JSON(data);
+        else
+            return {};
+    }
+})(fs);
